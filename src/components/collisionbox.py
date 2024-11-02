57008487d5e2e2195f01ce34481384944d673e2e
nameless-lord <namelesslord.dev@gmail.com>
2024-11-02 17:47:32 +0200

    fix collision box scaling

--- src/components/collisionbox.py
+++ src/components/collisionbox.py
@@ -20,12 +20,14 @@ class CollisionBox(ActorComponent):
 
     def on_debug_draw(self, frame_time: float) -> None:
         self.update_position()
-        pyray.draw_rectangle_lines_ex(self._rectangle, 2, pyray.GREEN)
+        pyray.draw_rectangle_lines_ex(self._rectangle, 4, pyray.GREEN)
 
 
     def update_position(self) -> None:
-        self._rectangle.x = self._actor.position.x - self.width * 0.5
-        self._rectangle.y = self._actor.position.y - self.height * 0.5
+        self._rectangle.x = self.actor.position.x - self.width * 0.5
+        self._rectangle.y = self.actor.position.y - self.height * 0.5
+        self._rectangle.width = self.width * self.actor.scale.x
+        self._rectangle.height = self.height * self.actor.scale.y
 
 
     def check_collision_point(self, point: Vector2) -> bool:
