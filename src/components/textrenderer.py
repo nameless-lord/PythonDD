2e08af8d3e9dd967bf4fafb08e9e6497bf13d4d4
nameless-lord <namelesslord.dev@gmail.com>
2024-11-14 11:38:18 +0200

    update text renderer

--- src/components/textrenderer.py
+++ src/components/textrenderer.py
@@ -5,21 +5,30 @@ from core import *
 
 class TextRenderer(ActorComponent):
 
-    def __init__(self, text: str, offset: Vector2, font_size: int = 24, color: Color = pyray.WHITE):
+    def __init__(self, text: str, centered: bool = True, font_size: int = 24, spacing: int = 4, tint: Color = Color.WHITE):
         super().__init__()
-        self.text = text
-        self.offset = offset
-        self.font_size = font_size
-        self.color = color
+        self.text: str = text
+        self.centered: bool = centered
+        self.font_size: int = font_size
+        self.spacing: int = spacing
+        self.tint: Color = tint
 
 
     def on_draw(self, frame_time: float) -> None:
         font: pyray.Font = pyray.get_font_default()
+
+        origin: pyray.Vector2 = pyray.vector2_zero()
+
+        if self.centered:
+            origin = pyray.measure_text_ex(font, self.text, self.font_size, self.spacing)
+            origin = pyray.vector2_scale(origin, 0.5)
+
+
         pyray.draw_text_pro(font,
                             self.text,
-                            self._actor.position,
-                            pyray.vector2_negate(self.offset),
+                            self._actor.position.to_rl_vector(),
+                            origin,
                             self._actor.rotation,
                             self.font_size,
-                            4,
-                            self.color)
+                            self.spacing,
+                            self.tint)
