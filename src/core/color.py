453d3c402b29d3fa8c63e3f1f48ae7472f90dd09
nameless-lord <namelesslord.dev@gmail.com>
2024-11-14 11:37:45 +0200

    added color class

---
+++ src/core/color.py
@@ -0,0 +1,13 @@
+import pyray
+
+class Color:
+
+    WHITE = (255, 255, 255, 255)
+    RED = (255, 0, 0, 255)
+    BLACK = (0, 0, 0, 255)
+
+    def __init__(self, r, g, b, a):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
