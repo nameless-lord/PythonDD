7d6f354f73e4d36120b4fb20d181a0e4798584c9
nameless-lord <namelesslord.dev@gmail.com>
2024-11-14 11:37:10 +0200

    added cast from vector2 to raylibs vector

--- src/core/vector2.py
+++ src/core/vector2.py
@@ -1,4 +1,10 @@
+import pyray
+
 class Vector2:
     def __init__(self, x: float = 0, y: float = 0):
         self.x = x
         self.y = y
+
+
+    def to_rl_vector(self) -> pyray.Vector2:
+        return pyray.Vector2(self.x, self.y)
