06fc7e68147efc18710b8a9f7dbdc14cc5772792
nameless-lord <namelesslord.dev@gmail.com>
2024-11-22 21:50:41 +0200

    removed test scripts

--- src/components/destroyonstart.py
+++
@@ -1,7 +0,0 @@
-from core import *
-
-
-class DestroyOnStart(ActorComponent):
-
-    def on_start(self) -> None:
-        self._actor.destroy()
--- src/components/followcursor.py
+++
@@ -1,7 +0,0 @@
-from core import *
-import pyray
-
-class FollowCursor(ActorComponent):
-    def on_update(self, frame_time: float) -> None:
-        position = pyray.get_mouse_position()
-        self._actor.position = position
